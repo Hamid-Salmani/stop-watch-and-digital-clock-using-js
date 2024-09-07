398b8681d2017758a0ddf0383f4619ae6dfb2073
hamidreza <hamid.r.salmaniy@gmail.com>
2024-09-07 09:59:05 +0330

    the first version

---
+++ clock.js
@@ -0,0 +1,28 @@
+let sec = document.querySelector(".sec")
+let min = document.querySelector(".min")
+let hour = document.querySelector(".hour")
+let clock = document.querySelector(".clock")
+let stopwatch = document.querySelector(".watch")
+let clockBtn = document.querySelector(".clockSelect")
+let stopwatchBtn = document.querySelector(".stopwatchsel")
+let Btns = document.querySelector(".Btns")
+
+clockBtn.addEventListener('click' , ()=>{
+    stopwatch.classList = 'watch'
+    clock.classList = 'clock'
+    Btns.classList = 'Btns'
+})
+stopwatchBtn.addEventListener('click' , () =>{
+    clock.classList = 'clockIvs'
+    stopwatch.classList = 'stopwatchVs'
+    Btns.classList = 'button'
+})
+
+setInterval(timehandler , 1000)
+function timehandler(){
+    let date = new Date()
+    sec.innerHTML = date.getSeconds()
+    min.innerHTML = date.getMinutes()
+    hour.innerHTML = date.getHours()
+
+}
